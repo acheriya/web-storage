c6f5c381f0676b931af826c935b46349bdbc9f37
exProtocol <exprotocol@gmail.com>
2017-05-16 17:20:01 -0400

    Added AES Encryption

--- web-storage.js
+++ web-storage.js
@@ -1,12 +1,35 @@
-var Storage = function() {
-  this.firstName = "";
+/*
+WebStorage v1.1.0
+(c) 2016-2017 by - Anas KVC. All rights reserved. 
+https://exprotocol.com/
+*/
+
+
+var Storage = function(encrptionKey) {
+  this.encrptionKey = encrptionKey || null;
+}
+
+Storage.prototype.encrypt = function(data) {
+    return CryptoJS.AES.encrypt(data, this.encrptionKey).toString();
+}
+
+Storage.prototype.decrypt = function(data) {
+    return CryptoJS.AES.decrypt(data, this.encrptionKey).toString(CryptoJS.enc.Utf8);
 }
 
 Storage.prototype.create = function(name, data) {  
+    
     if (localStorage.getItem(name) === null) {
-        localStorage.setItem(name, JSON.stringify(data) || "");
+
+        data = JSON.stringify(data) || "";
+
+        if(this.encrptionKey!= null)
+            data = this.encrypt(data);
+
+        localStorage.setItem(name, data);
     }
 }
+
 
 Storage.prototype.drop = function(name)
 {
@@ -16,10 +39,29 @@ Storage.prototype.drop = function(name)
 
 Storage.prototype.get = function(name) {
 
-    var item = localStorage.getItem(name);
+    var data = localStorage.getItem(name);
 
-    if (item != null) 
-    	return JSON.parse(item);
+    if (data != null) 
+    {
+       if(this.encrptionKey!= null)
+            data = this.decrypt(data);
+
+        return JSON.parse(data);
+    }
+    	
+}
+
+Storage.prototype.update = function(name, data) {  
+    
+    if (localStorage.getItem(name) != null) {
+
+        data = JSON.stringify(data) || "";
+
+        if(this.encrptionKey!= null)
+            data = this.encrypt(data);
+
+        localStorage.setItem(name, data);
+    }
 }
 
 
